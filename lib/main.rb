fa1579eb4809d3ddbb9fb20b37cecbb9d316476e
Vladyslav Protsenko <vladyslav.protsenko@ucu.edu.ua>
2024-11-02 20:33:16 +0000

    Added logger

---
+++ lib/main.rb
@@ -0,0 +1,7 @@
+require_relative 'logger/logger'
+require 'yaml'
+
+config_path = './config/logging.yaml'
+MyApplicationName::LoggerManager.setup(config_path)
+MyApplicationName::LoggerManager.log_processed_file('Processed a file successfully.')
+MyApplicationName::LoggerManager.log_error('An error occurred while processing the file.')
