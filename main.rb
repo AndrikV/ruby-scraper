dcc2308fcb0ef41d26afc13c594b7f143387fbf1
AndrikV <andriutsyo@gmail.com>
2024-11-09 16:14:38 +0200

    частина 3.3, клас Configurator

---
+++ main.rb
@@ -0,0 +1,21 @@
+require_relative 'app_config_loader'
+
+app_config = AppConfigLoader.new
+app_config.load_libs('lib')
+app_config.config('config/default_config.yaml', 'config/yaml')
+app_config.pretty_print_config_data()
+MyApplicationName::LoggerManager.setup(app_config.config_data['logging'])
+MyApplicationName::LoggerManager.log_processed_file('Processed a file successfully.')
+MyApplicationName::LoggerManager.log_error('An error occurred while processing the file.')
+
+# Тестування класу Configurator
+configurator = Configurator.new
+configurator.configure(
+  run_website_parser: 1,
+  run_save_to_csv: 1,
+  run_save_to_yaml: 1,
+  run_save_to_sqlite: 1,
+  invalid_key: 1  # Невалідний ключ
+)
+puts configurator.config
+puts Configurator.available_methods
